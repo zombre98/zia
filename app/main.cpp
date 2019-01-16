5fca113a983414670b4554fba702d3257b1802ce
zombre <thomas.burgaud@epitech.eu>
2019-01-16 22:27:11 +0100

    Move Main and add logger

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
+
+#define ASIO_STANDALONE 1
+#include "server/asio/AsioServer.hpp"
+#include "server/IClient.hpp"
+
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
-	return 0;
+  nkpp::AsioServer serv("0.0.0.0", 4242);
+
+  serv.whenOnConnected([](nkpp::IClient &client) {
+    client.whenOnRead([](nkpp::Buffer &){
+      std::cout << "Reading !" << std::endl;
+    });
+  });
+  serv.whenOnDisconnected([](nkpp::IClient &){
+    std::cout << "He is disconnected !" << std::endl;
+  });
+  serv.run();
+  return 0;
 }
---
+++ src/Utils/Logger.hpp
@@ -0,0 +1,31 @@
+//
+// Created by armandmgt on 12/3/18.
+//
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#define LOG_DEBUG "[" << __FILE__ << ":" << __LINE__ << "] "
+#define LOG_TIME "[" << __TIME__ << ":" << __LINE__ << "] "
+
+namespace logging {
+	template<typename T>
+	struct logger : T {
+		explicit logger(std::streambuf *sb) :
+				std::ostream{sb} {
+		};
+
+		explicit logger(std::ofstream &&rhs) :
+				std::ofstream{std::move(rhs)} {
+		};
+	};
+
+	inline logger<std::ostream> debug(std::cout.rdbuf());
+
+	inline logger<std::ofstream> info(std::ofstream("log.txt"));
+
+	inline logger<std::ostream> error(std::cerr.rdbuf());
+
+}
