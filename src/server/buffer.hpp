30ce4216f71ae3642e3ddb72b824cda5a80b01ee
Khalif <bilel.fourati@epitech.eu>
2019-01-18 19:18:39 +0100

    Buffer: Editing the writing and reading system, now using a cursor.

--- src/server/buffer.hpp
+++ src/server/buffer.hpp
@@ -9,13 +9,14 @@ class Buffer {
   using bufferContainer = std::vector<uint8_t>;
   static constexpr int32_t BUFFER_SIZE = 1024;
 public:
-  Buffer() {
-   buffer_.resize(BUFFER_SIZE);
+  Buffer() : cursor_(0) {
+    buffer_.resize(BUFFER_SIZE);
   }
 
 private:
   void eraseFront(size_t size) {
     buffer_.erase(buffer_.begin(), buffer_.begin() + size);
+    cursor_ -= size;
   }
 
 public:
@@ -23,10 +24,12 @@ public:
   void setData(T *ptr, size_t size) {
     buffer_.resize(size);
     ::memcpy(buffer_.data(), ptr, size);
+    cursor_ += size;
   }
 
 public:
   void clear() {
+    cursor_ = 0;
     buffer_.clear();
     buffer_.resize(BUFFER_SIZE);
   }
@@ -42,7 +45,8 @@ public:
 public:
   template <typename T, typename = typename std::enable_if_t<std::is_arithmetic<T>::value, T>>
   void write(T data) {
-    buffer_.insert(buffer_.end(), &data, &data + sizeof(T));
+    buffer_.insert(buffer_.begin() + cursor_, &data, &data + sizeof(T));
+    cursor_ += sizeof(T);
   }
 
   template <typename T, typename = typename std::enable_if_t<std::is_arithmetic<T>::value, T>>
@@ -60,7 +64,8 @@ public:
   template <typename T, typename = typename std::enable_if_t<std::is_same<std::string, T>::value, T>>
   void write(std::string data) {
     // write<size_t>(data.size());
-    buffer_.insert(buffer_.end(), data.c_str(), data.c_str() + data.size());
+    buffer_.insert(buffer_.begin() + cursor_, data.c_str(), data.c_str() + data.size());
+    cursor_ += data.size();
     for (auto &it : buffer_)
 	    logging::debug << static_cast<char>(it);
     std::cout << std::endl;
@@ -78,6 +83,7 @@ public:
 
 private:
   bufferContainer buffer_;
+  size_t cursor_;
 };
 
 }
