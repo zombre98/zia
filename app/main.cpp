0fe93a2899c37ae86fa16185169b9d58658ee136
zombre <thomas.burgaud@epitech.eu>
2019-01-22 10:28:21 +0100

    Main without response

--- app/main.cpp
+++ app/main.cpp
@@ -5,18 +5,18 @@
 #include "server/asio/AsioServer.hpp"
 #include "server/IClient.hpp"
 
-
 int main() {
   nkpp::AsioServer serv("0.0.0.0", 4242);
 
   serv.whenOnConnected([](nkpp::IClient &client) {
     client.whenOnRead([&client](nkpp::Buffer &b){
       logging::debug << LOG_TIME << b.read<std::string>() << std::endl;
-        nkpp::Buffer sendBuffer;
-        sendBuffer.write<std::string>("HTTP/1.1 200\r\n");
-        sendBuffer.write<std::string>("Host: 0.0.0.0:4242\r\n\r\n");
-        sendBuffer.write<std::string>("<html><h1>Coucou</h1></html>");
-        client.write(sendBuffer);
+/*      nkpp::Buffer sendBuffer;
+      sendBuffer.write<std::string>("HTTP/1.1 200\r\n");
+      sendBuffer.write<std::string>("Content-Length: 28\r\n");
+      sendBuffer.write<std::string>("Host: 0.0.0.0:4242\r\n\r\n");
+      sendBuffer.write<std::string>("<html><h1>Coucou</h1></html>\r\n\r\n");
+      client.write(sendBuffer);*/
     });
   });
   serv.whenOnDisconnected([](nkpp::IClient &client){
