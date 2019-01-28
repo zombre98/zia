1e37f7590a2372b730ad01ff4175226283b44556
zombre <thomas.burgaud@epitech.eu>
2019-01-28 21:45:38 +0100

    Add enum documentation

--- src/header/HeaderEnum.hpp
+++ src/header/HeaderEnum.hpp
@@ -18,6 +18,14 @@ namespace http {
 		PATCH
 	};
 
+	/**
+	 * Enum for HTTP status code
+	 * 1xx: Informational - Request received, continuing process
+   * 2xx: Success - The action was successfully received, understood, and accepted
+   * 3xx: Redirection - Further action must be taken in order to complete the request
+   * 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
+   * 5xx: Server Error - The server failed to fulfill an apparently valid request
+	 */
 	enum class StatusCode {
 		Continue = 100,
 		Switching_Protocols = 101,
