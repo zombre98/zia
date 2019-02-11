9fc3427e742aff3c29cf6d368365bdbce2bba031
Khalif <bilel.fourati@epitech.eu>
2019-02-11 18:59:12 +0100

    Some doc and merge swith patch master

--- src/server/Modules/TestModule.cpp
+++ src/server/Modules/TestModule.cpp
@@ -12,23 +12,23 @@ extern "C" {
  * @param manager The Stage manager to hook the module
  */
 void registerHooks(dems::StageManager &manager) {
-	manager.connection().hookToFirst(MODULE_NAME, [](dems::Context &ctx) {
-		logging::debug << LOG_DEBUG << "Stage: Request FIRST" << std::endl;
-		//logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
-		return dems::CodeStatus::OK;
-	});
+  manager.request().hookToFirst(1,MODULE_NAME, [](dems::Context &ctx) {
+      logging::debug << LOG_DEBUG << "Stage: Request FIRST" << std::endl;
+      //logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
+      return dems::CodeStatus::OK;
+      });
 
-	manager.connection().hookToMiddle(MODULE_NAME, [](dems::Context &ctx) {
-		logging::debug << LOG_DEBUG << "Stage: Request MIDDLE" << std::endl;
-		//logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
-		return dems::CodeStatus::OK;
-	});
+  manager.request().hookToMiddle(1, MODULE_NAME, [](dems::Context &ctx) {
+      logging::debug << LOG_DEBUG << "Stage: Request MIDDLE" << std::endl;
+      //logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
+      return dems::CodeStatus::OK;
+      });
 
-	manager.connection().hookToEnd(MODULE_NAME, [](dems::Context &ctx) {
-		logging::debug << LOG_DEBUG << "Stage: Request END";
-		//logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
-		return dems::CodeStatus::OK;
-	});
+  manager.request().hookToEnd(1, MODULE_NAME, [](dems::Context &ctx) {
+      logging::debug << LOG_DEBUG << "Stage: Request END";
+      //logging::debug << LOG_DEBUG << ctx.response.body << std::endl;
+      return dems::CodeStatus::OK;
+      });
 }
 
 };
