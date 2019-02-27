6de08f3bee9ca79629380d05955a318be6639861
zombre <thomas.burgaud@epitech.eu>
2019-02-27 21:58:55 +0100

    Remove code duplication

--- src/server/Modules/LoadPathModule.cpp
+++ src/server/Modules/LoadPathModule.cpp
@@ -22,16 +22,16 @@ std::string registerHooks(dems::StageManager &manager) {
 		auto path = root + std::get<dems::header::Request>(ctx.request.firstLine).path;
 		if (std::filesystem::is_directory(path)) {
 		  auto &defaultFile = std::get<std::string>(ctx.config["default_file"].v);
-		  std::ifstream errorStream(root + defaultFile);
-		  std::string s((std::istreambuf_iterator<char>(errorStream)), std::istreambuf_iterator<char>());
-		  ctx.response.body = s;
-		  ctx.response.firstLine = dems::header::Response{"HTTP/1.1", "200", ""};
-		  ctx.response.headers->setHeader("Content-Length", std::to_string(ctx.response.body.length()));
-		  return dems::CodeStatus::OK;
+		  path = root + defaultFile;
 		}
+		std::cout << "Open path : " << path << std::endl;
 		std::ifstream fStream(path);
 		std::string s((std::istreambuf_iterator<char>(fStream)), std::istreambuf_iterator<char>());
 		ctx.response.body = s;
+		return dems::CodeStatus::OK;
+	});
+
+	manager.request().hookToEnd(1, MODULE_NAME, [](dems::Context &ctx) {
 		ctx.response.firstLine = dems::header::Response{"HTTP/1.1", "200", ""};
 		ctx.response.headers->setHeader("Content-Length", std::to_string(ctx.response.body.length()));
 		return dems::CodeStatus::OK;
