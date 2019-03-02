7235ba0a0cb38fc555caf97367199469af800856
zombre <thomas.burgaud@epitech.eu>
2019-03-02 12:35:32 +0100

    Before use this tuto : https://wiki.openssl.org/index.php/Simple_TLS_Server

--- src/server/Modules/openSslModule.cpp
+++ src/server/Modules/openSslModule.cpp
@@ -27,8 +27,6 @@ std::string registerHooks(dems::StageManager &manager) {
 		SSL_CTX  *ssl_ctx;
 		SSL  *myssl;
 
-		int listenerSocket;
-
 		SSL_library_init();
 		SSL_METHOD const *meth = SSLv23_server_method();
 		if (!(ssl_ctx = SSL_CTX_new(meth)))
@@ -41,12 +39,26 @@ std::string registerHooks(dems::StageManager &manager) {
 		if (SSL_CTX_use_PrivateKey_file(ssl_ctx, key_path.c_str(), SSL_FILETYPE_PEM) <= 0)
 			throw std::runtime_error("Error while using the private key");
 		if (SSL_CTX_check_private_key(ssl_ctx) == 0)
-			throw std::runtime_error("Cetificat and key doesn't match");
+			throw std::runtime_error("Certificat and key doesn't match");
 		SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
 		if (SSL_CTX_load_verify_locations(ssl_ctx, cert_path.c_str(), NULL) < 1)
 			throw std::runtime_error("Setting the verify location");
-		if (SSL_CTX_load_and_set_client_CA_file(ssl_ctx, cert_path.c_str()) < 1)
-			throw std::runtime_error("Error setting CA file");
+		//	if (SSL_CTX_load_and_set_client_CA_file(ssl_ctx, cert_path.c_str()) < 1)
+		// throw std::runtime_error("Error setting CA file");
+
+		myssl = SSL_new(ssl_ctx);
+		if (!myssl)
+			throw std::runtime_error("SSL_new failed");
+		SSL_set_fd(myssl, ctx.socketFd);
+		if (SSL_accept(myssl) < 1)
+			throw std::runtime_error("SSL accept failed");
+		if (SSL_get_verify_result(myssl) != X509_V_OK)
+			throw std::runtime_error("SSL Client Authentication error");
+		std::cout << "SSL connection on socket " << ctx.socketFd << ", Version: " << SSL_get_version(myssl) << ", Cipher: " << SSL_get_cipher(myssl) << std::endl;
+		char buff[1024];
+		if (SSL_read(myssl, buff, sizeof(buff)) < 1)
+			throw std::runtime_error("SSL read failed");
+		std::cout << "Receive from client :" << std::endl << buff << std::endl;
 
 		return dems::CodeStatus::OK;
 	});
