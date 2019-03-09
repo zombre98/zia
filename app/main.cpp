9c97fbffed5f4b16446fb47df77ad3f4e52bb9a7
zombre <thomas.burgaud@epitech.eu>
2019-03-09 16:37:11 +0100

    Handle bad request and move firstHook before read

--- app/main.cpp
+++ app/main.cpp
@@ -14,21 +14,26 @@ int main() {
 
   serv.whenOnConnected([&serv](zia::IClient &client) {
     client.whenOnRead([&client, &serv](zia::Buffer &b){
+			for (auto &first : serv.getModulesManager().getStageManager().request().firstHooks()) {
+				first.second.callback(client.getContext());
+			}
       auto data = b.read<std::string>();
       auto &context = client.getContext();
+			context.socketFd = client.getRawSocket();
 
 			std::copy(data.begin(), data.end(), std::back_inserter(context.rawData));
 			dems::header::fillHeading(data, context, *context.request.headers);
-			context.socketFd = client.getRawSocket();
-      for (auto &first : serv.getModulesManager().getStageManager().request().firstHooks()) {
-        first.second.callback(client.getContext());
-      }
-      for (auto &middle : serv.getModulesManager().getStageManager().request().middleHooks()) {
-        middle.second.callback(client.getContext());
-      }
-      for (auto &end : serv.getModulesManager().getStageManager().request().endHooks()) {
-        end.second.callback(client.getContext());
-      }
+			auto &request = std::get<dems::header::Request>(context.request.firstLine);
+			if (request.method.empty() && request.path.empty() && request.httpVersion.empty())
+				context.response.firstLine = dems::header::Response {"HTTP/1.1", "400", "Bad Request"};
+			else {
+				for (auto &middle : serv.getModulesManager().getStageManager().request().middleHooks()) {
+					middle.second.callback(client.getContext());
+				}
+				for (auto &end : serv.getModulesManager().getStageManager().request().endHooks()) {
+					end.second.callback(client.getContext());
+				}
+			}
       auto response = dems::header::constructResponse(client.getContext());
       zia::Buffer responseBuffer;
       responseBuffer.write<std::string>(response);
