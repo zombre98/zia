9f9392e9142fbd3b3c4d0a62f2803bf8c95602c0
zombre <thomas.burgaud@epitech.eu>
2019-03-09 19:06:04 +0100

    Add test with multiple headers

--- tests/Context.cpp
+++ tests/Context.cpp
@@ -41,3 +41,19 @@ TEST(Context, Context_construct_from_string_with_headers) {
 	EXPECT_EQ("HTTP/1.1\r", firstline.httpVersion);
 	EXPECT_EQ("value", context.request.headers->getHeader("key"));
 }
+
+TEST(Context, Context_construct_from_string_with_multiple_headers) {
+	std::string stringHeader("GET /index.html HTTP/1.1\r\n"
+													 "key: value,othervalue\r\n"
+													 "otherkey: anothervalue\r\n");
+	dems::Context context;
+	context.request.headers = std::make_unique<dems::header::Heading>();
+
+	dems::header::fillHeading(stringHeader, context, *context.request.headers);
+	auto &firstline = std::get<dems::header::Request>(context.request.firstLine);
+	EXPECT_EQ("GET", firstline.method);
+	EXPECT_EQ("/index.html", firstline.path);
+	EXPECT_EQ("HTTP/1.1\r", firstline.httpVersion);
+	EXPECT_EQ("value,othervalue", context.request.headers->getHeader("key"));
+	EXPECT_EQ("anothervalue", context.request.headers->getHeader("otherkey"));
+}
